7b5240516be3a6e5cc19a052452c8fb0599daa23
Nazar <autoservicetrue@gmail.com>
2021-01-13 20:03:18 +0200

    work with objects

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
 "use strict";
 
+const numberOfFilms = prompt("How many films have you seen?","");
 
-const first = '4';
-console.log(typeof (first));
 
-const hello = prompt("Ви ще на сайті?", "18");
-console.log(typeof (hello));
 
+const personalMovieDB = {
+    count: `${numberOfFilms}`,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+}
+
+const lastFilm = prompt("Last film?","");
+const rateFilm = prompt("Film rating:","");
+const a = prompt("Last film?","");
+const b = prompt("Film rating:","");
+
+
+personalMovieDB.movies = {
+[lastFilm]:rateFilm,
+    [a]:b
+}
+
+personalMovieDB.movies[lastFilm] = rateFilm;
+personalMovieDB.movies[a] = b;
+
+console.log(personalMovieDB)
