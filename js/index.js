d62f3125d597974543358ffe851c560deb4ca274
Nazar <autoservicetrue@gmail.com>
2021-01-13 22:53:00 +0200

    learn conditions

--- js/index.js
+++ js/index.js
@@ -1,29 +1,36 @@
 "use strict";
 
-const numberOfFilms = prompt("How many films have you seen?","");
+while(true) {
+
+    const numberOfFilms = prompt("How many films have you seen?", "");
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        private: false
+    }
+
+    console.log(personalMovieDB.count)
 
 
-
-const personalMovieDB = {
-    count: `${numberOfFilms}`,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
+    if (personalMovieDB.count < 10 && personalMovieDB.count !=null && personalMovieDB.count !== "") {
+        console.log('Мало фільмів.')
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log('Класичний глядач.')
+    } else if (personalMovieDB.count > 30) {
+        console.log('Ви кіноман.')
+    } else if (personalMovieDB.count === "") {
+        console.log('Помилка');
+    } else if (personalMovieDB.count === null || personalMovieDB.count === "") {
+        console.log('Помилка');
+    }
+    else if (personalMovieDB.count === 'stop') {
+        break;
+    }
 }
 
-const lastFilm = prompt("Last film?","");
-const rateFilm = prompt("Film rating:","");
-const a = prompt("Last film?","");
-const b = prompt("Film rating:","");
 
 
-personalMovieDB.movies = {
-[lastFilm]:rateFilm,
-    [a]:b
-}
 
-personalMovieDB.movies[lastFilm] = rateFilm;
-personalMovieDB.movies[a] = b;
 
-console.log(personalMovieDB)
