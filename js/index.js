5182eb4a8ea773631e4b6a70f2f7aad9f63b29c8
Nazar <autoservicetrue@gmail.com>
2021-01-14 16:42:06 +0200

    work with functions

--- js/index.js
+++ js/index.js
@@ -1,36 +1,29 @@
 "use strict";
 
-while(true) {
-
-    const numberOfFilms = prompt("How many films have you seen?", "");
-    const personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        private: false
-    }
-
-    console.log(personalMovieDB.count)
-
-
-    if (personalMovieDB.count < 10 && personalMovieDB.count !=null && personalMovieDB.count !== "") {
-        console.log('Мало фільмів.')
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-        console.log('Класичний глядач.')
-    } else if (personalMovieDB.count > 30) {
-        console.log('Ви кіноман.')
-    } else if (personalMovieDB.count === "") {
-        console.log('Помилка');
-    } else if (personalMovieDB.count === null || personalMovieDB.count === "") {
-        console.log('Помилка');
-    }
-    else if (personalMovieDB.count === 'stop') {
-        break;
-    }
+let personalMovieDB = {
+    count: 5,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
 }
 
+function showMyDb(personalMovieDB) {
+if (personalMovieDB.private === false) {
+    console.log(personalMovieDB)
+}
+}
+
+showMyDb(personalMovieDB);
 
 
+const writeYourGenres = (personalMovieDB) => {
+    for (let i=0; i<3; i++) {
+         let question = prompt(`Ваш любимий жанр по номеру: ${i+1}?`);
+        personalMovieDB.genres.push(question);
+    }
 
+}
 
+writeYourGenres(personalMovieDB);
+console.log(personalMovieDB);
