a55de27f93d9139f41957be2c98d93aaaa512743
Nazar <autoservicetrue@gmail.com>
2021-01-16 18:05:43 +0200

    DOM practice

--- js/index.js
+++ js/index.js
@@ -1,37 +1,66 @@
-"use strict";
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const array = [...movieDB.movies]
+console.log(array.sort())
 
 
-const box = document.getElementById('box'),
-    buttons = document.getElementsByTagName('button'),//[1] індекс елемента з псевдомассиву
-    circles = document.getElementsByClassName('circle'),
-    hearts = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelector('.heart'),
-wrapper = document.querySelector('.wrapper');
+const promo = document.querySelector('.promo__adv')
 
+const img = promo.querySelectorAll('.promo__adv img')
 
-buttons[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
-
-box.style.cssText = 'background-color: blue; width: 60vw';
-
-/*
-for (let i=0; i<hearts.length; i++) {
-    hearts[i].style.backgroundColor='orange'
-}*/
-
-hearts.forEach(item => {
-    item.style.backgroundColor = 'orange';
+img.forEach(item => {
+ item.remove()
 })
 
-const div = document.createElement('div');
-//const text = document.createTextNode('Hello world');
+const promoG = document.querySelector('.promo__genre')
 
-div.classList.add('black');
+promoG.textContent="Драма";
 
-/*circles[0].remove();*/
+const poster = document.querySelector('.promo__bg')
+poster.style.background = 'url("img/bg.jpg") center center/cover no-repeat';
 
-document.body.append(div)
+const promoList = document.querySelector('.promo__interactive-list');
+const promoItem = document.querySelectorAll('.promo__interactive-item');
 
-div.innerHTML = "<h1>hello<h2/>";
+promoItem.forEach(item => {
+    item.remove()
+})
 
-div.insertAdjacentHTML('beforebegin', "<h2>Hello world!</h2>")
+//We can do this practice using innerHtml=""...
+promoItem.forEach(( item, i) => {
+    item[i] = document.createElement('li');
+    item[i].classList.add('promo__interactive-item')
+    let deleteList = document.createElement('div');
+    deleteList.classList.add('delete')
+    item[i].textContent = `${i+1 }  ${array[i]}`;
+    promoList.append(item[i])
+    item[i].append(deleteList)
+})
+
+
+
+
