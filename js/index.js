16e1b0f2ece5f2c7e3a10cfdab56299cc3a26f9e
Nazar <autoservicetrue@gmail.com>
2021-01-16 19:16:26 +0200

    event listener

--- js/index.js
+++ js/index.js
@@ -1,66 +1,29 @@
-/* Задания на урок:
+"use strict"
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+const btn = document.querySelector('button'),
+    overlay = document.querySelector('.overlay');
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту
-
-5) Добавить нумерацию выведенных фильмов */
-
-'use strict';
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const array = [...movieDB.movies]
-console.log(array.sort())
+let i = 0;
+const deleteElement = (e) => {
+console.log(e.target)
+console.log(e.type)
+    i++;
+/*
+if(i === 1) {
+        btn.removeEventListener('click',  deleteElement)
+    }
+*/
+}
 
 
-const promo = document.querySelector('.promo__adv')
 
-const img = promo.querySelectorAll('.promo__adv img')
+btn.addEventListener('click',  deleteElement,  {once:true})
+overlay.addEventListener('click', deleteElement, {once:true})
 
-img.forEach(item => {
- item.remove()
+
+const link = document.querySelector('a')
+link.addEventListener('click', (event)=> {
+    event.preventDefault();
+
+    console.log(event.target)
 })
-
-const promoG = document.querySelector('.promo__genre')
-
-promoG.textContent="Драма";
-
-const poster = document.querySelector('.promo__bg')
-poster.style.background = 'url("img/bg.jpg") center center/cover no-repeat';
-
-const promoList = document.querySelector('.promo__interactive-list');
-const promoItem = document.querySelectorAll('.promo__interactive-item');
-
-promoItem.forEach(item => {
-    item.remove()
-})
-
-//We can do this practice using innerHtml=""...
-promoItem.forEach(( item, i) => {
-    item[i] = document.createElement('li');
-    item[i].classList.add('promo__interactive-item')
-    let deleteList = document.createElement('div');
-    deleteList.classList.add('delete')
-    item[i].textContent = `${i+1 }  ${array[i]}`;
-    promoList.append(item[i])
-    item[i].append(deleteList)
-})
-
-
-
-
